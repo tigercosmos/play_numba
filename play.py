2836b73057fa83ff44e20463c88079b10d70d5c3
dboyliao <qmalliao@gmail.com>
2023-03-06 21:04:35 +0800

    daily update

--- play.py
+++ play.py
@@ -1,12 +1,10 @@
-import os
+import sysconfig
+from pathlib import Path
 
 import numpy as np
 from llvmlite import binding
 from numba import jit
 from numba.core import types, typing
-import sysconfig
-
-x = np.arange(100).reshape(10, 10)
 
 
 @jit(nopython=True)  # Set "nopython" mode for best performance, equivalent to @njit
@@ -17,11 +15,11 @@ def go_fast(a):  # Function is compiled to machine code when called the first ti
     return a + trace  # Numba likes NumPy broadcasting
 
 
-print(go_fast(x))
-
 # load the library into LLVM
-path = os.path.abspath(f"libexample{sysconfig.get_config_var('EXT_SUFFIX')}")
-binding.load_library_permanently(path)
+lib_path = Path(f"libexample{sysconfig.get_config_var('EXT_SUFFIX')}")
+if not lib_path.exists():
+    raise ValueError(f"extension lib not found: {lib_path}")
+binding.load_library_permanently(str(lib_path.absolute()))
 
 # Adds typing information
 c_func = types.ExternalFunction(
@@ -30,8 +28,18 @@ c_func = types.ExternalFunction(
 
 
 @jit(nopython=True)
-def example(x, y):
+def _example(x: int, y: int):
     return c_func(x, y)
 
 
-print(example(3, 4))
+def example(x: int, y: int):
+    if not isinstance(x, int):
+        x = int(x)
+    if not isinstance(y, int):
+        y = int(y)
+    return _example(x, y)
+
+
+if __name__ == "__main__":
+    print(go_fast(np.arange(100).reshape(10, 10)))
+    print(example(3, 4))
